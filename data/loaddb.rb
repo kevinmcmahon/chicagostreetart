f34cd9b1dc80e1247c964e5152d27ef5e431c917
Kevin McMahon <klmcmahon+github@gmail.com>
2012-04-25 10:17:01 -0500

    update page title and some meta data

--- data/loaddb.rb
+++ data/loaddb.rb
@@ -5,7 +5,7 @@ require 'data_mapper'
 require './photo.rb'
 
 DataMapper::Logger.new($stdout, :debug)
-DataMapper::setup(:default,ENV['DATABASE_URL'])
+DataMapper::setup(:default,ENV['DATABASE_URL'] || 'postgres://localhost/streetart')
 DataMapper.finalize
 
 FlickRaw.api_key=ENV['FLICKR_API']
@@ -24,19 +24,18 @@ def getUrl(p)
   ''
 end
 
-(1..7).each do |page|
-  photos = flickr.photos.search :page => page, :group_id => '95553424@N00', :has_geo => '1' , :extras =>'description,url_l,url_m,url_s,url_z,geo', :per_page => 500, :format => 'json', :nojsoncallback => 1
-
+(1..13).each do |page|
+  photos = flickr.photos.search :page => page, :group_id => '95553424@N00', :has_geo => '1' , :extras =>'description,url_l,url_m,url_s,url_z,geo', :per_page => 250, :format => 'json', :nojsoncallback => 1, :min_date_upload => 946706400
   photos.each do |p|
     url = getUrl(p)
     ap p
-    # photo = Photo.first_or_create(
-    #                  :photo_id => p.id,
-    #                  :title => p.title,
-    #                  :description => p.description,
-    #                  :url => url,
-    #                  :latitude => p.latitude,
-    #                  :longitude => p.longitude
-    #                )
+    photo = Photo.first_or_create(
+                     :photo_id => p.id,
+                     :title => p.title,
+                     :description => p.description,
+                     :url => url,
+                     :latitude => p.latitude,
+                     :longitude => p.longitude
+                   )
   end     
 end
